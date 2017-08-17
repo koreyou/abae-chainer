79d5c37592ccc2feb692a20da706153984341e91
Yuta Koreeda <koreyou@mac.com>
2017-08-17 12:54:32 +0900

    Added more options to CLI.
    Also changed some option default to be consistant with the original
    papser.

--- bin/train.py
+++ bin/train.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 @click.command()
-@click.option('--epoch', '-e', type=int, default=20,
+@click.option('--epoch', '-e', type=int, default=15,
               help='Number of sweeps over the dataset to train')
 @click.option('--frequency', '-f', type=int, default=-1,
               help='Frequency of taking a snapshot')
@@ -33,10 +33,15 @@ logging.basicConfig(level=logging.INFO)
 @click.option('--ntopics', '-n', type=int, default=14)
 @click.option('--train_ratio', type=float, default=0.95,
               help='Number of data to be used for validation')
+@click.option('--lr', type=float, default=0.001, help='Learning rate')
+@click.option('--orthogonality_penalty', type=float, default=1.0,
+              help='Orthogonality penalty coefficient lambda')
+@click.option('--fix_embedding', type=bool, default=True,
+              help='Fix word embedding during training')
 @click.option('--resume', '-r', default='',
               help='Resume the training from snapshot')
 def run(epoch, frequency, gpu, out, word2vec, batchsize, negative_samples,
-        ntopics, train_ratio, resume):
+        ntopics, train_ratio, lr, orthogonality_penalty, fix_embedding, resume):
     memory = Memory(cachedir=out, verbose=0)
 
     @memory.cache
@@ -51,15 +56,15 @@ def run(epoch, frequency, gpu, out, word2vec, batchsize, negative_samples,
 
     w2v, vocab, dataset, topic_vectors = prepare(word2vec, ntopics)
 
-    model = abae.model.ABAE(w2v, topic_vectors, fix_embedding=False,
-                            orthogonality_penalty=1.0)
+    model = abae.model.ABAE(w2v, topic_vectors, fix_embedding=fix_embedding,
+                            orthogonality_penalty=orthogonality_penalty)
     if gpu >= 0:
         # Make a specified GPU current
         chainer.cuda.get_device_from_id(gpu).use()
         model.to_gpu()  # Copy the model to the GPU
 
     # Setup an optimizer
-    optimizer = chainer.optimizers.Adam()
+    optimizer = chainer.optimizers.Adam(alpha=lr)
     optimizer.setup(model)
 
     train, test = chainer.datasets.split_dataset_random(dataset, int(len(dataset) * train_ratio))
